b118036a0066b4e9742319030afc34179eae49e8
Carlos Planchart <carlos_planchart@live.com>
2022-06-13 10:58:51 +0200

    Host binding added to favorite directive

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,8 +1,19 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
+//hostBinding relates to detecting when a mouse hovers over an object.
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  @HostListener('mouseenter') onMouseEnter() {
+      this.hovering = true;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hovering = false;
+}
+  @Input() set mwFavorite(value) {
+      this.isFavorite = value;
+  }
 }
