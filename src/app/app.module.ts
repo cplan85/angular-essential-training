b7ce813b22ad5111903c14601f1ac8e30af84d22
Carlos Planchart <carlos_planchart@live.com>
2022-06-30 10:22:58 +0200

    imported httpxhrbackend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { MockXHRBackend } from './mock-xhr-backend';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
@@ -14,7 +15,7 @@ import { lookupListToken, lookupLists } from './providers';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   declarations: [
     AppComponent,
@@ -25,7 +26,8 @@ import { lookupListToken, lookupLists } from './providers';
     MediaItemFormComponent
   ],
   providers: [
-    { provide: lookupListToken, useValue: lookupLists }
+    { provide: lookupListToken, useValue: lookupLists },
+    { provide: HttpXhrBackend, useClass: MockXHRBackend}
   ],
   bootstrap: [
     AppComponent
